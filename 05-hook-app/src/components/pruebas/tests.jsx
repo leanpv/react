ffba227b88a9aebc161feb14ecdc713b508944b7
Leandro <lean.puebla@gmail.com>
2020-10-07 20:56:57 -0300

    fix push

---
+++ 05-hook-app/src/components/pruebas/tests.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './tests.css'
+
+export const Tests = () => {
+    return (
+        <div className="arrow-up">
+            aca deberia estar
+        </div>
+    )
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -7,9 +7,10 @@ import ReactDOM from 'react-dom';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import {FocusScreen} from './components/04-useRef/FocusScreen'
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Tests } from './components/pruebas/tests';
 
 ReactDOM.render(
-    <Layout />,
+    <Tests />,
     document.getElementById('root')
 );
